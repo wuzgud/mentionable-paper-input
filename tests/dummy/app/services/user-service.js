765d56367e8ce9372d9944dc9ee25b6b417a86cd
Andrew Ball <ball.andrew.j@gmail.com>
2020-03-06 12:33:53 -0500

    cannot use test helpers in dummy app so moved test user objects to user-service

--- tests/dummy/app/services/user-service.js
+++ tests/dummy/app/services/user-service.js
@@ -1,14 +1,52 @@
 import Service from '@ember/service';
 import RSVP from 'rsvp';
-import { getBigArrayTestUsers } from '../../../helpers/get-test-users';
+import { User } from 'tests/dummy/app/models/user';
 
 export default class UserService extends Service {
+  users = [
+    {
+      "username": "ajball",
+      "name":"Andrew Ball"
+    },
+    {
+      "username": "janine",
+      "name": "Janine Henry"
+    },
+    {
+      "username": "will",
+      "name":"Will Henry"
+    },
+    {
+      "username": "slauna",
+      "name": "Shauna Robertson"
+    },
+    {
+      "username": "wsb",
+      "name":"Will Belcher"
+    },
+    {
+      "username": "chuck_city",
+      "name": "Charlie Arrendale"
+    },
+    {
+      "username": "carly",
+      "name":"Carly Waters"
+    },
+    {
+      "username": "test",
+      "name": "Test User"
+    }
+  ];
   constructor() {
     super(...arguments);
   }
   findAll(searchTerm) {
     return new RSVP.Promise(
-      resolve => resolve( searchTerm ? getBigArrayTestUsers() : [] )
+      resolve => resolve(
+        searchTerm
+        ? this.users.map( user => new User({ name: user.name, username: user.username }) )
+        : []
+      )
     );
   }
 }
