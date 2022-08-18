e657b40ed1edf7fdd5194e4bd43365e9b1e76bba
andrewball <andrew@returnkey.co>
2022-08-18 09:38:26 -0400

    ember-source upgrade to 3.28.0 from 3.24.0

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -1,12 +1,12 @@
 'use strict';
 
 module.exports = {
-  extends: 'octane',
+  extends: 'recommended',
   rules: {
     'no-implicit-this': {
       allow: ['noop']
     },
     'no-curly-component-invocation': { allow: ['mentionable-paper-input', 'option-result', 'mention-option'] },
     'no-html-comments': false
-  }
+  },
 };
